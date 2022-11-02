6e6dc999d4ed5b137d2f2abb7d5bd93e9a8af554
David Griffin <dgriffin@arpio.io>
2022-11-01 17:59:24 -0700

    Implement semi-working command execution (works for ./hello.sh but not ls).

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::process::Command;
+use std::str;
 
 use rusqlite::{Connection, Result};
 
@@ -15,11 +17,22 @@ fn main() -> Result<()> {
     let conn = Connection::open_in_memory()?;
     conn.execute("CREATE TABLE log (id INTEGER PRIMARY KEY, output TEXT, input TEXT NOT NULL)", ())?;
     let args: Vec<String> = env::args().collect();
+    let mut cmd = Command::new("sh");
+    cmd.args(&args[1..]);
+    dbg!(&cmd.get_args());
+    let output = cmd.output().unwrap();
+    dbg!(&output.stdout);
+    dbg!(&output.stderr);
+    let out_str = str::from_utf8(&output.stdout).unwrap();
+    let err_str = str::from_utf8(&output.stderr).unwrap();
+    dbg!(out_str);
+    dbg!(err_str);
     let one = Record {
         id: 0,
         input: args.join(" "),
-        output: Some("hello.txt".to_string()),
+        output: Some(String::from(out_str)),
     };
+   
     conn.execute("INSERT INTO log (input, output) VALUES (?1, ?2)",
 		 (&one.input, &one.output))?;
     let mut stmt = conn.prepare("SELECT id, input, output FROM log")?;
